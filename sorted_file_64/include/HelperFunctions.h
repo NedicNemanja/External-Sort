0fd30c46797dbce96f60fb769746c5cea372cb3b
NedicNemanja <sdi1400124@di.uoa.gr>
2018-01-12 19:23:02 +0200

    ...

--- sorted_file_64/include/HelperFunctions.h
+++ sorted_file_64/include/HelperFunctions.h
@@ -44,7 +44,7 @@ int CopyFile(int fileDesc1, int fileDesc2);
 void quickSort(BF_Block** blockArray, int size, int fieldNo, int lastRun, int lastRunSize);
 
 //load a group of runs to the buffer
-void PinGroup(Run** pinnedRuns,int in_file,int* current_block_id,int run_size,
+int PinGroup(Run** pinnedRuns,int in_file,int* current_block_id,int run_size,
                   int* num_of_unmerged_blocks,int lastRunSize,int bufferSize);
 
 //undoes PinGroup()
