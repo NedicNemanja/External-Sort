6a873d972966f116a624d4dd2ca001fed2179ce4
NedicNemanja <sdi1400124@di.uoa.gr>
2018-01-13 05:08:23 +0200

    lastrunsize wasnt being updated wehn runsize chaged

--- sorted_file_64/src/sort_file.c
+++ sorted_file_64/src/sort_file.c
@@ -329,6 +329,7 @@ fflush(stdout);
     //number of run-groups
     int groups_in_file = (int)ceil((double)runs_in_file/(double)(bufferSize-1));
 printf("runs_in_file:%d,groups_in_file:%d\n", runs_in_file,groups_in_file);
+printf("run_size:%d,lastRunSize:%d\n", run_size,lastRunSize);
 fflush(stdout);
     /*load,sort,store all the groups one by one*/
     for(int g=0; g<groups_in_file; g++){
@@ -362,6 +363,7 @@ fflush(stdout);
       SR_OpenFile(output_filename,&out_file);
     //runs have been merged in groups, the new run is a whole group
     run_size = run_size*(bufferSize-1);
+    lastRunSize = BlockCount%run_size;
 printf("Outfile after iteration:%d---------------------------------------------\n", iteration);
 //SR_PrintAllEntries(out_file);
   }
